63e8f4a887a3a9afbd89673bb0dfb7299c009fe9
Ryn-Cryp <riyanfirst13@gmail.com>
2023-07-25 21:14:27 +0700

    resource

--- app/src/main/java/com/ryan/belajarandroiddasar/MainActivity.kt
+++ app/src/main/java/com/ryan/belajarandroiddasar/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ryan.belajarandroiddasar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -25,11 +26,21 @@ class MainActivity : AppCompatActivity() {
 
             initComponent()
 
-            sayHelloTextView.text = "Hi"
+            sayHelloTextView.text = resources.getString(R.string.app_name)
 
             sayHelloButton.setOnClickListener {
+
+                Log.d("PZN", "This is debug Log")
+                Log.i("PZN", "This is Info Log")
+                Log.w("PZN", "This is Warnig Log")
+                Log.e("PZN", "This is error Log")
+
                 val name = nameEditText.text.toString()
-                sayHelloTextView.text = "Hi $name"
+                sayHelloTextView.text = resources.getString(R.string.sayHelloTextView, name)
+
+                resources.getStringArray(R.array.names).forEach {
+                    Log.i("PZN", it)
+                }
             }
         }
     }
