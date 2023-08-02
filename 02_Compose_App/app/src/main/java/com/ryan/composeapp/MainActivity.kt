86485846abdd5af1efe850f46d95eb81dd4e351c
Ryn-Cryp <riyanfirst13@gmail.com>
2023-08-02 21:16:32 +0700

    new:(learn how to use Column and Row at jetpack compose)

--- 02_Compose_App/app/src/main/java/com/ryan/composeapp/MainActivity.kt
+++ 02_Compose_App/app/src/main/java/com/ryan/composeapp/MainActivity.kt
@@ -3,44 +3,61 @@ package com.ryan.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ryan.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .border(width = 1.dp, color = Color.Red)
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Greeting("Android")
+                    Text(text = "Jetpack Compose")
+                    Text(
+                        text = "With Riyan",
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                }
+                Spacer(modifier = Modifier.padding(16.dp))
+                Row(
+                    modifier = Modifier
+                        .border(width = 1.dp, color = Color.Blue)
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Jetpack Compose",
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                    Text(text = "With Riyan")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeAppTheme {
-        Greeting("Android")
     }
 }
